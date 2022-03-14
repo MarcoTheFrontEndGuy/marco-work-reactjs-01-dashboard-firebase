1f2f99d2b2d4f9bbaf205d51318d44391abe6d55
Marco <marco_trabajo@yahoo.com>
2022-03-14 14:09:10 +0100

    changes on topbar and giving main content scroll, but hide scrollbar

--- src/Components/MainTopBar.js
+++ src/Components/MainTopBar.js
@@ -9,21 +9,51 @@ function MainTopBar() {
   useEffect( () => {
 
    const userProfileMenuToggle = document.querySelector(".FaAngleDown-icon");
-   const userProfileBar__menu = document.querySelector(".userProfileBar__menu");
+   const userProfileBar__menu = document.querySelector(".userProfileBar__menu-ul");
+   
+// #############################################################################
+//  ----show/hide profile menu on click changing property right value 
+// function profileMenu_on_of() {
+//   if(userProfileBar__menu.style.right === "0px") {
+//     userProfileMenuToggle.style.transform = "Rotate(0deg)";
+//     userProfileBar__menu.style.right = "-200px";
+//   }else {
+//     userProfileBar__menu.style.right = "0px"; 
+//     userProfileMenuToggle.style.transform = "Rotate(180deg)";
+//   }
+// }
 
-   function profileMenu_on_of() {
-      if(userProfileBar__menu.style.right == "-10px") {
-        userProfileMenuToggle.style.transform = "Rotate(0deg)";
-        userProfileBar__menu.style.right = "-250px";
-      }else {
-        userProfileBar__menu.style.right = "-10px"; 
-        userProfileMenuToggle.style.transform = "Rotate(180deg)";
-      }
-   }
+// userProfileMenuToggle.addEventListener("click", profileMenu_on_of);
+//  END ----show/hide profile menu on click changing property right value 
+// #############################################################################
+
+
+// #############################################################################
+// SHOW PROFILE MENU WHEN MOUSE ENTER THE TOGGLE ICON 
+// HIDE PROFILE MENU WHEN THE MOUSE LEAVE THE PROFILE MENU 
+
+  function showProfileMenu() {
+    // userProfileBar__menu.style.right = "0px";
+    userProfileBar__menu.style.transform = "translateX(0px)";
+    userProfileMenuToggle.style.transform = "rotate(180deg)";
+    
+  }
+  
+  userProfileMenuToggle.addEventListener("mouseenter", showProfileMenu)
+  
+  
+  function hideProfileMenu() {
+    // userProfileBar__menu.style.right = "-200px";
+    userProfileBar__menu.style.transform = "translateX(300px)";
+    userProfileMenuToggle.style.transform = "rotate(0deg)";
+  }
+  
+  userProfileBar__menu.addEventListener("mouseleave", hideProfileMenu);
+
+// #############################################################################
 
 
 
-   userProfileMenuToggle.addEventListener("click", profileMenu_on_of);
 
 
 
@@ -44,21 +74,22 @@ function MainTopBar() {
           <div className="userProfileBar__notificationBell--wrapper">
             <FaBell className="userProfileBar__notificationBell"/> 
           </div> 
-          <div className="userProfile__userImage-container">
+
+          <div className="userProfileBar__userImage-container">
             <img className="userProfileBar__userImage" src={women} alt="logged in user image" />
           </div>
+
           <p className="userProfileBar__userName">Olivia Christine</p>
          
-          <div className="userProfileBar__menu-container">    
-            <FaAngleDown className="FaAngleDown-icon"/>
-            <ul className="userProfileBar__menu">             
-              <li>Web Design</li>
-              <li>UI/UX</li>
-              <li>Form Design</li>
-              <li>UI Design</li>
-            </ul>
-          
-          </div>
+          <FaAngleDown className="FaAngleDown-icon"/>
+        
+          <ul className="userProfileBar__menu-ul">             
+            <li>Web Design</li>
+            <li>UI/UX</li>
+            <li>Form Design</li>
+            <li>UI Design</li>
+          </ul>            
+
 
       </div>
     </div>
