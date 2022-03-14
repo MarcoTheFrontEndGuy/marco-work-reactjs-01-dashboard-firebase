044f98f5057f7affd5b2d213e5a12c6b63d17632
Marco <marco_trabajo@yahoo.com>
2022-03-14 15:20:23 +0100

    banner is finished

--- src/Components/MainBanner.js
+++ src/Components/MainBanner.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import './MainBanner.css'
+import Banner from "../img/1.jpg";
 
 function MainBanner() {
   return (
-    <div className="mainBanner">
+    <div className="mainBanner" style={{ 
+      background:`url(${Banner})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+      backgroundSize: "cover"
+    
+    }}>
       <div className="mainBanner__content">
-       <h1>Round Hall</h1>
-       <h2>1.5 ETH</h2>
-       <p>Uploaded by Alexander Vernof</p>
-       <div className="mainBanner__bid">
-        <button className="mainBanner__bid__button">BID NOW</button>
-        <p className="mainBanner__bid__dateline">Ending In <span>2d:15h:20m</span></p>
-       </div>
+        <h1>Round Hall</h1>
+        <h2>1.5 ETH</h2>
+        <p>Uploaded by Alexander Vernof</p>
+        <div className="mainBanner__bid">
+          <button className="mainBanner__bid__button">BID NOW</button>
+          <p className="mainBanner__bid__dateline">Ending In <span>2d:15h:20m</span></p>
+        </div>
       </div>
     </div>
   )
